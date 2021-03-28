c0fe7748a2026714edf1e77694b1beb0668e9a80
René Larch <renelarch@gmail.com>
2021-03-28 11:48:08 +0200

    diagnostic

--- TcBuildGenerator/Messages.cs
+++ TcBuildGenerator/Messages.cs
@@ -3,7 +3,7 @@
 
 namespace TcBuildGenerator {
     public static class Messages {
-        public static string[] Plugins = {
+        public static string[] PluginBaseClasses = {
             "TcPluginBase.FileSystem.FsPlugin",
             "TcPluginBase.Content.ContentPlugin",
             "TcPluginBase.Lister.ListerPlugin",
@@ -14,7 +14,7 @@ namespace TcBuildGenerator {
         public static DiagnosticDescriptor NoPluginError => new(
             id: "TcBuild0001",
             title: "No Plugin Found",
-            messageFormat: $"No plugin could be found. Please add a class that derives from a plugin base class like {string.Join(", ", Plugins)}",
+            messageFormat: $"No plugin could be found. Please add a class that derives from a plugin base class like {string.Join(", ", PluginBaseClasses)}",
             category: "Category",
             defaultSeverity: DiagnosticSeverity.Error,
             isEnabledByDefault: true
